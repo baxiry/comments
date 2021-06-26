fdbdf3143e2e16e8ee2b545cd973ec7eef2d02e3
fedor <ahmedalbachiry@gmail.com>
2021-06-26 19:51:00 +0300

    serve home page from '/' endpoint and add comment_handler.go file

---
+++ handle_comment.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+
+func index(c echo.Context) error {
+    return c.Render(http.StatusOK, "index.html", "Adamin")
+    // if err != nil {fmt.Println(err); return nil}; return nil;
+}
+
+
+
+func saveComment(c echo.Context) error {
+    name := c.FormValue("name")
+    comment := c.FormValue("comment")
+    fmt.Println("name: ",name, "\ncomment", comment)
+    return c.Render(http.StatusOK, "index.html", "admin")
+}
+
--- main.go
+++ main.go
@@ -1,27 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 )
-
-func index(c echo.Context) error {
-    return c.Render(http.StatusOK, "index.html", "Adamin")
-    // if err != nil {fmt.Println(err); return nil}; return nil;
-}
-
-
-
-func saveComment(c echo.Context) error {
-    name := c.FormValue("name")
-    comment := c.FormValue("comment")
-    fmt.Println("name: ",name, "\ncomment", comment)
-    return c.Render(http.StatusOK, "index.html", "admin")
-}
 
 
 func main() {
@@ -42,7 +25,8 @@ func main() {
     e.Static("/a", assets())
     e.Static("/fs", photoFold())
 
-    e.GET("/", homePage)//index)
+    e.GET("/home", homePage)//index)
+    e.GET("/", index)
     e.POST("/", saveComment)
     
     // account and verefy

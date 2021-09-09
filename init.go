c5c634e9519a17032706de8e8b271542a1f70831
fedora <fedora@fedoras-MacBook-Air.local>
2021-09-10 00:59:23 +0300

    big bug in this machine

--- init.go
+++ init.go
@@ -18,37 +18,35 @@ var (
 	err error
 )
 
-
 func setdb() *sql.DB {
 	db, err = sql.Open(
 		"mysql", "root:123456@tcp(127.0.0.1:3306)/?charset=utf8&parseTime=True&loc=Local")
 	if err != nil { // why no error when db is not runinig ??
-        fmt.Println("error when open mysql server", err)
+		fmt.Println("error when open mysql server", err)
 		// TODO report this error.
-        os.Exit(1)
+		os.Exit(1)
 	}
 
 	if err = db.Ping(); err != nil {
-        fmt.Println("error when ping to database", err)
-        switch {
-        case strings.Contains(err.Error(), "connection refused"):
-            // TODO handle errors by code of error not by strings.
+		fmt.Println("error when ping to database", err)
+		switch {
+		case strings.Contains(err.Error(), "connection refused"):
+			// TODO handle errors by code of error not by strings.
 
-            cmd := exec.Command("sudo", "service", "mariadb", "start") 
-            //cmd.Stdin = strings.NewReader(os.Getenv("JAWAD"))                        
-            errc := cmd.Run()                                                     
-            if errc != nil {                                                      
-                fmt.Println("error when run database cmd ", errc)                                                   
-            }
-        default:
-            fmt.Println("error at  setdb() func, db.Ping() func")
-            fmt.Println("unknown this error", err)
-            os.Exit(1)
-        }
-    }
+			cmd := exec.Command("sudo", "service", "mariadb", "start")
+			//cmd.Stdin = strings.NewReader(os.Getenv("JAWAD"))
+			errc := cmd.Run()
+			if errc != nil {
+				fmt.Println("error when run database cmd ", errc)
+			}
+		default:
+			fmt.Println("error at  setdb() func, db.Ping() func")
+			fmt.Println("unknown this error", err)
+			os.Exit(1)
+		}
+	}
 	return db
 }
-
 
 type Template struct {
 	templates *template.Template
@@ -60,20 +58,19 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 
 // path file is depends to enveronment.
 func templ() *Template {
+	// TODO reset tmpl file paht to compatible with this machine
 	var p string
-	if os.Getenv("USERNAME") != "fedor" {
+	if os.Getenv("USERNAME") != "fedora" {
 		p = "/root/store/"
 	}
 	files := []string{
-        p + "tmpl/home.html",p + "tmpl/upacount.html", p + "tmpl/acount.html",
-        p + "tmpl/login.html", p + "tmpl/sign.html", p + "tmpl/404.html", p + "tmpl/upphotos.html",
-        p + "tmpl/upcomment.html", p + "tmpl/comment.html", p + "tmpl/notfound.html",p + "tmpl/post.html", 
+		p + "tmpl/home.html", p + "tmpl/upacount.html", p + "tmpl/acount.html",
+		p + "tmpl/login.html", p + "tmpl/sign.html", p + "tmpl/404.html", p + "tmpl/upphotos.html",
+		p + "tmpl/upcomment.html", p + "tmpl/comment.html", p + "tmpl/notfound.html", p + "tmpl/post.html",
 		p + "tmpl/upload.html", p + "tmpl/part/header.html", p + "tmpl/part/footer.html",
-
 	}
 	return &Template{templates: template.Must(template.ParseFiles(files...))}
 }
-
 
 //  get path of photo folder
 func photoFold() string {
@@ -83,7 +80,6 @@ func photoFold() string {
 	return "/root/files/"
 }
 
-
 //  assets return path assets.
 func assets() string {
 	if os.Getenv("USERNAME") != "fedor" {
@@ -91,4 +87,3 @@ func assets() string {
 	}
 	return "assets"
 }
-

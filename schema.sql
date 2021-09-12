49cb49d9f2ea4288e8fc217ddaf7beaf6f01f5ac
bashery <ahmedalbachiry@gmail.com>
2021-09-12 23:33:38 +0300

    create some tables

--- schema.sql
+++ schema.sql
@@ -1,15 +1,21 @@
-CREATE TABLE comments.posts (postid INT NOT NULL AUTO_INCREMENT, title TINYTEXT, post TEXT, timestamp TIMESTAMP, primary key (postid));
-insert into posts(title, post) values('Title of best article in the world', 'this is my an awesome article you read in your life. if you do not know then go to hell ok ? thins your welcome');
+CREATE TABLE users (
+    userid int unsigned NOT NULL AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
+    avatarlink varchar(300),
+    PRIMARY KEY (userid)
+);
 
-
-
-insert into comment(link, parent_id, commentor_name, comment_text) values('localhost:1323', 1, 'jawad', 'this is my 4 comment ');
-
-UPDATE comment SET commentor_name = 'hamed', column2 = value WHERE id > 1 and id < 4;
-
-ALTER TABLE comment ALTER comment_id SET DEFAULT NOT NULL;
-
-ALTER table posts ADD COLUMN ownerid int unsigned;
+CREATE TABLE comments (
+    userId int unsigned NOT NULL,
+    commentId int unsigned NOT NULL,
+    parentId int unsigned DEFAULT 0,
+    comment text NOT NULL,
+    creatAt TIMESTAMP,
+    editedAt TIMESTAMP,
+    PRIMARY KEY (userid)
+);
 
 Schema::create('comments', function (Blueprint $table) {
        $table->increments('id');
@@ -20,4 +26,32 @@ Schema::create('comments', function (Blueprint $table) {
        $table->string('commentable_type');
        $table->timestamps();
 });
+
+
+
+
+CREATE TABLE comments.posts (
+    postid INT NOT NULL AUTO_INCREMENT,
+    title TINYTEXT, post TEXT,
+    timestamp TIMESTAMP,
+    primary key (postid)
+);
+
+
+
+insert into posts(title, post) values(
+    'Title of best article in the world',
+    'this is my an awesome article you read in your life.
+    if you do not know then go to hell ok ? thins your welcome'
+);
+
+
+
+insert into comment(link, parent_id, commentor_name, comment_text) values('localhost:1323', 1, 'jawad', 'this is my 4 comment ');
+
+UPDATE comment SET commentor_name = 'hamed', column2 = value WHERE id > 1 and id < 4;
+
+ALTER TABLE comment ALTER comment_id SET DEFAULT NOT NULL;
+
+ALTER table posts ADD COLUMN ownerid int unsigned;
 
